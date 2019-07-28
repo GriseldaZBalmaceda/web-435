4b56d5b0e40c0782183a2c45d5727d96dcaaf2ee
GriseldaZBalmaceda <gzbalmaceda@my365.bellevue.edu>
2019-07-28 16:27:59 -0500

    am not able to display app

---
+++ week-3/guards/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from '../app/home/home.component';
+import {LoginComponent} from   '../app/login/login.component';
+import {ProductDetailComponent} from '../app/product-detail/product-detail.component'
+
+const routes: Routes = [
+{path:'',component:HomeComponent},
+{path:'/login',component:LoginComponent},
+{path:'/productDetail',component:ProductDetailComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
