806e085ca884bb7532157f38314e21b407809938
GriseldaZBalmaceda <gzbalmaceda@my365.bellevue.edu>
2019-08-11 21:08:18 -0500

    commiting assignment 5.4

--- week-3/guards/src/app/login/login.component.ts
+++ week-3/guards/src/app/login/login.component.ts
@@ -11,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   template: `
-    <p>
-     Please login in here!
-    </p>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
   `,
   styles: []
 })
