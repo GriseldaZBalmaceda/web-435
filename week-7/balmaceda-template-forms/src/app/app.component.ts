a918d999756ad6ce64f59428be29d77105fa8d74
GriseldaZBalmaceda <gzbalmaceda@my365.bellevue.edu>
2019-08-24 13:32:30 -0500

    adding assignment 7.2

---
+++ week-7/balmaceda-template-forms/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'balmaceda-template-forms';
+  onSubmit(formData){
+    console.log(formData)
+  }
+}
